4e38cc3e76d0a4828d8f72422afa79f66bff47f0
Tomas <martin.tomas@courrier.uqam.ca>
2020-08-06 16:28:23 -0400

    Changes for local debugging only

--- FGC-Stream/main.cpp
+++ FGC-Stream/main.cpp
@@ -27,26 +27,28 @@ void printAllGens(GenNode* node) {
     for (auto child : *node->succ) {
         printAllGens(child.second);
     }
+    std::cout << node->clos->support << " ";
     std::set<uint32_t> itemset = node->items();
     for (auto item : itemset) {
         std::cout << item << " ";
     }
-    if (itemset.empty()) {
-        std::cout << "empty set";
-    }
+    //if (itemset.empty()) {
+    //    std::cout << "";//empty set
+    //}
     totalGens++;
     std::cout << "\n";
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
     // We get the first transaction manually, which is necessary as no itemset is present --> the algorithm will not work correctly
     // This means that it only works for minSupp = 1
     // TODO : get the intersection of the first minSupp transaction, then set root's closure as that intersection
     const uint32_t window_size = 1000;
-    minSupp = 1;
-    std::ifstream input("Datasets/test_input.txt");
+    if (argc < 2) return 1;
+    minSupp = strtoul(argv[2], 0, 10);//1;
+    std::ifstream input(/*"Datasets/in.txt"*/argv[1]);
     char s[10000];
     uint32_t i = 0;
     input.getline(s, 10000);
